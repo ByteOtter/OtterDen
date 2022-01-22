169ba1fc4a77cc9891e0afff5686d059e7bf18d9
Christopher Hock <christopher-hock@protonmail.com>
2022-01-22 01:36:35 +0100

    set up layout.html for unified layout

--- LogBlog/logBlog.py
+++ LogBlog/logBlog.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'About LogBlog')
 
 if __name__ == '__main__':
     app.run(debug=True)
