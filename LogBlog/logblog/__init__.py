dacb29e1fa7bdcd6c27779c9545375d25a257b83
Christopher Hock <christopher-hock@protonmail.com>
2022-01-23 19:12:46 +0100

    Add account info page, profile pictures and the possibility to change them.

--- LogBlog/logblog/__init__.py
+++ LogBlog/logblog/__init__.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 #secret key for tampering protection. 256 Bit python key from secrets module. THIS IS NOT A GOOD ENOUGH OF A SOLUTION.
 app.config['SECRET_KEY'] = '7e71d5735808a54d9a38d1ebc573ac1429242e464f7f322413a2c8e44392d37d11827c8792c81ca3ccc5cab4bbd5f2d3442823e6919ff74e3339199bd143e81da2e83ff986b2965742ae9b900b1811226b93b01d85ae3d86dd6f5ba6e7aea85d08bc6998f90402310c0ba86519a4d8d56eb5fb1bdcab1e90dc54cfde0164c89f60eeb198c74c813f3eb3e165e767f0d47aa8b1ba33c3fbe6461dc151430f7d46a5480306f87aaae953f5e2570d28663b6100ff06ed7c494c5cadcdac79b03d175288da284e327eab7c38f52b41a641176c66ece2b7083d180f7353f52adb42d6f897801c2639c1a5d6224db8b54985e5410e70b6e198c19de8fa549b474d1e43'
 
+#TODO: if database is not present. Have the app create a database file.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///logBlogDEV.db'
 #create SQLAlchemy database instance
 db = SQLAlchemy(app)
--- LogBlog/logblog/forms.py
+++ LogBlog/logblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from logblog.models import User
@@ -23,8 +25,30 @@ class RegistrationForm(FlaskForm):
         if email:
             raise ValidationError('An account is already registered with this email!')
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired(), Length(min = 5)])
     remember = BooleanField('Remember me?')
     submit = SubmitField('Sign In')
+
+
+class UpdateAccountInfoForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(min = 5, max = 20)])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    picture = FileField('Update Avatar', validators = [FileAllowed(['jpg', 'png', 'webp', 'gif'])])
+    submit = SubmitField('Update your info!')
+    
+    #check if username already in use
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('A user with this username already exists!')
+    
+    #check if email already in use
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('An account is already registered with this email!')
