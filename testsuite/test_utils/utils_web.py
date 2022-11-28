dc7e1de1bb88e78d6e08cb2b4282a904ad9febc3
ByteOtter <christopher-hock@protonmail.com>
2022-11-29 00:02:51 +0100

    add logfiles to gitignore

--- testsuite/test_utils/utils_web.py
+++ testsuite/test_utils/utils_web.py
@@ -1,7 +1,7 @@
 # Copyright ByteOtter (c) 2022
 
 from selenium import webdriver
-from utils_base import UtilFunc
+from test_utils.utils_base import UtilFunc
 
 def get_browser(browser):
     if browser == "firefox":
