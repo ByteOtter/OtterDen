0b95831872d01c5279bdf68631b638979fa1c292
ByteOtter <christopher-hock@protonmail.com>
2022-11-30 11:58:26 +0100

    fix pylint errors

--- LogBlog/logblog/__init__.py
+++ LogBlog/logblog/__init__.py
@@ -1,15 +1,16 @@
-#Copyright ByteOtter (c) 2021-2022
+# Copyright ByteOtter (c) 2021-2022
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+
 from logblog.config import Config
 
 #TODO: if database is not present. Have the app create a database file.
 
-#create SQLAlchemy database instance
+# create SQLAlchemy database instance
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
@@ -20,24 +21,24 @@ mail = Mail()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-    
-    #pass application to all extensions
+
+    # pass application to all extensions
     #-> No application specific config is stored on the extensions so mulitple configs can be used
     #-> allows to create multiple instances of the app with different configs
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    #import Blueprint instances
+    # import Blueprint instances
     from logblog.users.routes import users
     from logblog.posts.routes import posts
     from logblog.main.routes import main
-    #register error handlers with application
+    # register error handlers with application
     from logblog.errors.handlers import errors
-    #register Blueprints to app
+    # register Blueprints to app
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
-    
+
     return app
