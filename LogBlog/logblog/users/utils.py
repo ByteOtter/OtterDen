eabf5309c99e1e0ca3f9ac51b6918ab6d2751a66
Christopher Hock <christopher-hock@protonmail.com>
2022-11-30 22:21:28 +0100

    test labeler (#56)

--- LogBlog/logblog/users/utils.py
+++ LogBlog/logblog/users/utils.py
@@ -1,4 +1,4 @@
-#Copyright ByteOtter (c) 2021-2022
+# Copyright ByteOtter (c) 2021-2022
 
 import os
 import secrets
@@ -7,18 +7,18 @@ from flask import url_for, current_app
 from flask_mail import Message
 from logblog import mail
 
-#save image file the user updates. randomize the name to avoid cloissions with files the same name, split the extension off the filename and user "_" to throw away the unused filename
+# save image file the user updates. randomize the name to avoid cloissions with files the same name, split the extension off the filename and user "_" to throw away the unused filename
 def save_picture(form_picture):
     random_name = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_name + f_ext
-    #join together the package directory with the custom images location and the picture file name
+    # join together the package directory with the custom images location and the picture file name
     picture_path = os.path.join(current_app.root_path, 'static/profile_pictures', picture_fn)
-    #resize picture to improve performance and save space
+    # resize picture to improve performance and save space
     output_size = (300, 300)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
-    #save image to filesystem
+    # save image to filesystem
     i.save(picture_path)
 
     return picture_fn
