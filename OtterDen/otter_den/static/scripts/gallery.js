9e63c49fed43fd9593b4dbba89f27d41648a0cd9
fruitfly638 <moritz.kodytek@suse.com>
2023-01-31 15:24:34 +0100

    Enlarge Images on the posts page by clicking on them

---
+++ OtterDen/otter_den/static/scripts/gallery.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  var modal = document.getElementById("modal-img");
+  var img = document.getElementById("img-not-modal");
+  var modalImg = document.getElementById("img-modal");
+
+  img.addEventListener('click', function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    var span = document.getElementsByClassName('close')[0];
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  });
+});
+
+ 
