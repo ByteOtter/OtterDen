6606fd13d77a353dacb6bcd35039bf0f8d991ba7
ByteOtter <christopher-hock@protonmail.com>
2023-02-23 22:38:46 +0100

    have form send GET request

--- OtterDen/otter_den/main/forms.py
+++
@@ -1,9 +0,0 @@
-#Copyright ByteOtter (c) 2023
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Regexp
-
-class SearchForm(FlaskForm):
-    query = StringField('Search', validators = [DataRequired(), Regexp('[^\s]{5,20}', message="Username cannot contain whitespaces!")])
-    submit = SubmitField('Search')
--- OtterDen/otter_den/main/utils.py
+++ OtterDen/otter_den/main/utils.py
@@ -14,10 +14,15 @@ from otter_den.models import Post, User
 5. if topic, filter returned posts by topic
 """
 def search_db(query):
-  search_string = query;
+  print("Your original query:" + query)
   is_user_query = re.search("\buser:[\S]+", query)
   only_with_images = re.search("\bimage:true", query)
   topic_filter = re.search("\btopic:[\S]+", query)
+  # get the searchphrase itself without the flags to compare to posts if there are any flags that is
+  if is_user_query | only_with_images | topic_filter:
+    search_phrase = re.sub()
+  else:
+    search_phrase = query
 
   if is_user_query:
     # is_user_query.group() returns the string "user:<username>". Split and take 2nd element <username>
